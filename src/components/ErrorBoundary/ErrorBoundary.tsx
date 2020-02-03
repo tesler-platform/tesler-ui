82ac385d32635a12e3cb519647165bd23118bb50
kdergachev <dergachev@hotmail.com>
2020-02-03 16:24:33 +0300

    Error boundary wrapper to catch render errors

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+// tslint:disable:jsdoc-require
+interface ErrorBoundaryProps {
+    msg?: React.ReactNode
+}
+
+export default class ErrorBoundary extends React.Component<ErrorBoundaryProps, { hasError: boolean }> {
+
+    static getDerivedStateFromError(error: any) {
+        return { hasError: true }
+    }
+
+    constructor(props: ErrorBoundaryProps) {
+        super(props)
+        this.state = { hasError: false }
+    }
+
+    componentDidCatch(error: any, errorInfo: any) {
+        console.error(error)
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return this.props.msg || 'Render error occured'
+        }
+        return this.props.children
+    }
+}
