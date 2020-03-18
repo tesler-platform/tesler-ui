17d181a22f8161089996287a349304635e217c41
Губарев Федор Николаевич <fgubarev@phoenixit.ru>
2020-03-18 19:22:38 +0300

    added bcNames comparision (#144)

---
+++ src/middlewares/__tests__/autosaveMiddleware.test.ts
@@ -0,0 +1,43 @@
+import { createAutoSaveMiddleware } from '../autosaveMiddleware'
+import * as mockStore from './__mocks__/store.json'
+import * as mockStore1 from './__mocks__/store1.json'
+import { OperationTypeCrud } from 'interfaces/operation'
+import { AnyAction } from 'redux'
+
+describe('saveFormMiddleware', () => {
+    const initAction1 = {
+        type: 'sendOperation',
+        payload: {
+            bcName: '1EventRootReason',
+            operationType: 'save',
+            widgetName: '1 Event Root Reason Edit Form'
+        }
+    }
+    const initAction2 = {
+        type: 'sendOperation',
+        payload: {
+            bcName: '1EventRoot1Response',
+            operationType: OperationTypeCrud.cancelCreate,
+            widgetName: 'RERRRCF',
+        }
+    }
+    const doGetState = () => mockStore
+    const doGetState1 = () => mockStore1
+    const doNext = (action: AnyAction) => action
+    const autosaveMiddleware = createAutoSaveMiddleware()
+    test('should transform action', () => {
+        expect(autosaveMiddleware(
+            // @ts-ignore
+            {getState: doGetState}
+            )(doNext)(initAction1).payload.onSuccessAction
+        ).toBeTruthy()
+    })
+    test('should not transform action', () => {
+        expect(autosaveMiddleware(
+            // @ts-ignore
+            {getState: doGetState1}
+            )(doNext)(initAction2)
+        ).toEqual(initAction2)
+    })
+
+})
