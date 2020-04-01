2558bc346f9979132716d62a2502a7a830acd51c
Романов Сергей Михайлович <smromanov@phoenixit.ru>
2020-04-01 17:37:14 +0300

    new implementation of postInvokeConfirm

--- src/components/ModalInvoke/ModalInvoke.tsx
+++ src/components/ModalInvoke/ModalInvoke.tsx
@@ -2,43 +2,67 @@ import React from 'react'
 import {Dispatch} from 'redux'
 import {connect} from 'react-redux'
 import {Store} from '../../interfaces/store'
-import {Modal} from 'antd'
+import {Modal, Input} from 'antd'
 import {$do} from '../../actions/actions'
 import {useTranslation} from 'react-i18next'
-import {OperationPreInvoke} from 'interfaces/operation'
+import {OperationPostInvokeConfirmType, OperationPostInvokeConfirm} from '../../interfaces/operation'
+import styles from './ModalInvoke.less'
 
 interface ModalInvokeProps {
     bcName: string,
     operationType: string,
     widgetName: string,
-    confirmOperation: OperationPreInvoke
-    onOk: (bcName: string, operationType: string, widgetName: string, postInvokeConfirm: OperationPreInvoke) => void,
+    confirmOperation: OperationPostInvokeConfirm
+    onOk: (bcName: string, operationType: string, widgetName: string, confirm: string) => void,
     onCancel: () => void,
 }
 
 const ModalInvoke: React.FunctionComponent<ModalInvokeProps> = (props) => {
     const {t} = useTranslation()
-    const modal = Modal.confirm({
-        title: props.confirmOperation?.message || t('Perform an additional action?'),
-        content: t('Are you sure?'),
-        okText: t('Ok'),
-        cancelText: t('Cancel')
-    })
-    modal.update({
-        onOk: () => {
+    const [value, setValue] = React.useState('')
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value || null)
+    }
+
+    const getContent = () => {
+        switch (props.confirmOperation.type) {
+            case OperationPostInvokeConfirmType.confirm: {
+                return <div>
+                    <p>{props.confirmOperation?.messageContent || t('Are you sure?')}</p>
+                </div>
+            }
+            case OperationPostInvokeConfirmType.confirmText: {
+                return <div>
+                {props.confirmOperation?.messageContent && <p>{props.confirmOperation?.messageContent}</p>}
+                {<Input value={value} onChange={handleChange}/>}
+            </div>
+            }
+            default:
+                return null
+        }
+    }
+
+    return <Modal
+        className={styles.modal}
+        visible={true}
+        title={props.confirmOperation?.message || t('Perform an additional action?')}
+        okText={t('Ok')}
+        cancelText={t('Cancel')}
+        onOk={() => {
             props.onOk(
                 props.bcName,
                 props.operationType,
                 props.widgetName,
-                props.confirmOperation
+                value || 'ok'
             )
-            modal.destroy()
-        },
-        onCancel: () => {
+        }}
+        onCancel={() => {
             props.onCancel()
-        }
-    })
-    return null
+        }}
+    >
+        {getContent()}
+    </Modal>
 }
 
 function mapStateToProps(store: Store) {
@@ -53,8 +77,8 @@ function mapStateToProps(store: Store) {
 
 function mapDispatchToProps(dispatch: Dispatch) {
     return {
-        onOk: (bcName: string, operationType: string, widgetName: string, confirmOperation?: OperationPreInvoke) => {
-            dispatch($do.sendOperation({ bcName, operationType, widgetName, confirmOperation}))
+        onOk: (bcName: string, operationType: string, widgetName: string, confirm?: string) => {
+            dispatch($do.sendOperation({ bcName, operationType, widgetName, confirm}))
             dispatch($do.closeConfirmModal(null))
         },
         onCancel: () => {
