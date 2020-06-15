f6c1e00a94dbc776f97a1bfec6896e5ac78d71c4
Моргунов Андрей Александрович <andrewram33@gmail.com>
2020-06-15 14:59:55 +0300

    alpha version ^1.14.6-alpha.0 preInvoke middleware

--- src/middlewares/preInvokeMiddleware.tsx
+++ src/middlewares/preInvokeMiddleware.tsx
@@ -5,16 +5,28 @@
 import {AnyAction, Dispatch, Middleware, MiddlewareAPI} from 'redux'
 import {$do, types} from '../actions/actions'
 import {Store as CoreStore} from '../interfaces/store'
+import {buildBcUrl} from '..'
+import {isOperationGroup, Operation} from '../interfaces/operation'
 
 const preInvokeAction = ({getState, dispatch}: MiddlewareAPI<Dispatch<AnyAction>, CoreStore>) => (next: Dispatch) =>
     (action: AnyAction) => {
         if (action.type === types.sendOperation) {
+            const state = getState()
+            console.log(state)
             const bcName = action.payload.bcName
+            const bcUrl = buildBcUrl(bcName, true)
             const operationType = action.payload.operationType
             const widgetName = action.payload.widgetName
-            const preInvoke = action.payload?.confirmOperation
-
-            return preInvoke
+            const operations: Operation[] = []
+            state.view.rowMeta[bcName][bcUrl].actions.forEach(item => {
+                if (isOperationGroup(item)) {
+                    item.actions.forEach(operation => {operations.push(operation)})
+                } else {
+                    operations.push(item)
+                }
+            })
+            const preInvoke = operations?.filter(item => item.type === operationType && item?.preInvoke)[0]?.preInvoke
+            return preInvoke && !action.payload.confirm
                 ? next($do.processPreInvoke({
                     bcName,
                     operationType,
