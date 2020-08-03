af1c110b42fc98a6c43e3f1e994880f1cdc30cde
Fedor Gubarev <gubarevfyodor@gmail.com>
2020-08-03 15:10:48 +0300

    Refactored AutoSaveMiddleware (#376) Mechanism of middlewares combining is provided (#376) Added prop `tooltipPlacement` to `Field` component; Component AssocTable isn't sending the whole data to pendingDataChanges; initial code of `clearPendingDataChangesAfterCursorChange` epic is Deleted;

---
+++ src/utils/combineMiddlewares.ts
@@ -0,0 +1,54 @@
+import {Middleware} from 'redux'
+import {
+    CustomMiddlewares,
+    CoreMiddlewares,
+    CustomMiddleware,
+    CoreMiddlewareType, CoreMiddlewareOverrideDescriptors
+} from '../interfaces/customMiddlewares'
+
+export function combineMiddlewares(
+    coreMiddlewares: CoreMiddlewares,
+    customMiddlewares: CustomMiddlewares = null,
+) {
+    if (!customMiddlewares) {
+        return Object.values(coreMiddlewares)
+    }
+    const coreMiddlewaresKeys = Object.keys(coreMiddlewares) as CoreMiddlewareType[]
+
+    // get custom middlewares should be placed before core middlewares
+    const customMiddlewaresBefore: Middleware[] = []
+    const customMiddlewaresAfter: Middleware[] = []
+    Object.entries(customMiddlewares)
+    .filter(([cMKey, customMiddleware]) => !coreMiddlewaresKeys.includes(cMKey as keyof CoreMiddlewares))
+    .forEach(([cMKey, customMiddleware]) => {
+        if ((customMiddleware as unknown as CustomMiddleware)?.priority === 'BEFORE') {
+            customMiddlewaresBefore.push((customMiddleware as unknown as CustomMiddleware).implementation)
+            return
+        }
+        if ((customMiddleware as unknown as CustomMiddleware)?.priority === 'AFTER') {
+            customMiddlewaresAfter.push((customMiddleware as unknown as CustomMiddleware).implementation)
+        }
+    })
+
+
+    // if there are present customMiddlewaresBefore then insert them before core middlewares
+    const resultMiddlewares: Middleware[] = customMiddlewaresBefore
+
+    // replace or disable core middlewares by custom
+    coreMiddlewaresKeys.forEach(coreMiddlewareName => {
+        const customMiddleware = (customMiddlewares as CoreMiddlewareOverrideDescriptors)[coreMiddlewareName]
+        // Null values means disabling the core implementation of middleware
+        if (customMiddleware === null) { return }
+        // Missing custom implementation means the core implementation will be used
+        if (!customMiddleware) {
+            resultMiddlewares.push(coreMiddlewares[coreMiddlewareName])
+            return
+        }
+        // Assigned custom implementation means it'll override the core implementation
+        if (customMiddleware) {
+            resultMiddlewares.push(customMiddleware as Middleware)
+            return
+        }
+    })
+    return [ ...resultMiddlewares, ...customMiddlewaresAfter ]
+}
