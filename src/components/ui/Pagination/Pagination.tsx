af3c96ab6335ab2f694188c0d9db25aaa97f8079
dergash <dergachev@hotmail.com>
2020-09-02 00:11:05 +0300

    Pagination component tests and refactoring

--- src/components/ui/Pagination/Pagination.tsx
+++ src/components/ui/Pagination/Pagination.tsx
@@ -1,20 +1,60 @@
+/*
+ * TESLER-UI
+ * Copyright (C) 2018-2020 Tesler Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React from 'react'
 import {PaginationMode} from '../../../interfaces/widget'
 import {Button} from 'antd'
-import {connect} from 'react-redux'
-import {Dispatch} from 'redux'
+import {useDispatch, useSelector} from 'react-redux'
 import {useTranslation} from 'react-i18next'
 import {Store} from '../../../interfaces/store'
 import styles from './Pagination.less'
 import {$do} from '../../../actions/actions'
 
+/**
+ * Pagination component properties
+ *
+ * TODO: Rename PaginataionProps in 2.0.0
+ */
 export interface PaginationOwnProps {
-    bcName: string,
+    /**
+     * Business component storing pagination data and loading state
+     *
+     * @deprecated TODO: Remove in favor of widgetName in 2.0.0
+     */
+    bcName?: string,
+    /**
+     * Name of the widget showing pagination
+     *
+     * TODO: Will be mandatory in 2.0.0
+     */
     widgetName?: string,
+    /**
+     * Type of paginator (prev/next buttons, loadMore button, etc.)
+     */
     mode: PaginationMode,
-    onChangePage?: () => void
+    /**
+     * Callback on page change
+     */
+    onChangePage?: (newPage?: number) => void
 }
 
+/**
+ * @deprecated Connected internally
+ */
 interface PaginationStateProps {
     hasNext: boolean,
     page: number,
@@ -22,95 +62,92 @@ interface PaginationStateProps {
     widgetName: string
 }
 
+/**
+ * @deprecated Connected internally
+ */
 interface PaginationDispatchProps {
     changePage: (bcName: string, page: number) => void,
     loadMore: (bcName: string, widgetName: string) => void
 }
 
-type PaginationAllProps = PaginationOwnProps & PaginationStateProps & PaginationDispatchProps
+// TODO: Leave only own props in 2.0.0
+type PaginationAllProps = PaginationOwnProps & Partial<PaginationStateProps> & Partial<PaginationDispatchProps>
 
+/**
+ * Pagination component for tables displaying business component's data
+ *
+ * Depending on the display mode, fires `bcLoadMore` or `bcChangePage` action
+ */
 const Pagination: React.FunctionComponent<PaginationAllProps> = (props) => {
-    // disable pagination if not required
-    if (!props.hasNext && props.page < 2) {
-        return null
-    }
+
+    const bcName = props.bcName // TODO: get only from store in 2.0.0
+        || useSelector((store: Store) => store.view.widgets.find(item => item.name === props.widgetName)?.bcName)
+    const hasNext = useSelector((store: Store) => store.screen.bo.bc[bcName]?.hasNext)
+    const page = useSelector((store: Store) => store.screen.bo.bc[bcName]?.page)
+    const loading = useSelector((store: Store) => store.screen.bo.bc[bcName]?.loading)
+    const dispatch = useDispatch()
+
     const onLoadMore = React.useCallback(
         () => {
-            props.loadMore(props.bcName, props.widgetName)
+            dispatch($do.bcLoadMore({ bcName, widgetName: props.widgetName }))
+            props.onChangePage?.(page + 1)
         },
-        [props.bcName, props.widgetName, props.loadMore]
+        [bcName, props.widgetName, page]
     )
 
     const onPrevPage = React.useCallback(
         () => {
-            props.changePage(props.bcName, props.page - 1)
+            const newPage = page - 1
+            dispatch($do.bcChangePage({ bcName, page: newPage }))
             if (props.onChangePage) {
-                props.onChangePage()
+                props.onChangePage(newPage)
             }
         },
-        [props.bcName, props.page, props.changePage]
+        [bcName, page]
     )
 
     const onNextPage = React.useCallback(
         () => {
-            props.changePage(props.bcName, props.page + 1)
+            const newPage = page + 1
+            dispatch($do.bcChangePage({ bcName, page: newPage }))
             if (props.onChangePage) {
-                props.onChangePage()
+                props.onChangePage(newPage)
             }
         },
-        [props.bcName, props.page, props.changePage]
+        [bcName, page]
     )
 
     const {t} = useTranslation()
 
-    if (props.mode === PaginationMode.page) {
-        return <div className={styles.paginationContainer}>
+    const isRequired = hasNext || props.mode === PaginationMode.page && page > 1
+
+    if (!isRequired) {
+        return null
+    }
+
+    return props.mode === PaginationMode.page
+        ? <div className={styles.paginationContainer}>
             <Button
                 className={styles.prevButton}
-                disabled={props.page < 2}
+                disabled={page < 2}
                 onClick={onPrevPage}
                 icon="left"
             />
             <Button
-                disabled={!props.hasNext}
+                disabled={!hasNext}
                 onClick={onNextPage}
                 icon="right"
             />
         </div>
-    } else {
-        return (props.hasNext)
-            ? <div className={styles.paginationContainer}>
-                <Button
-                    onClick={onLoadMore}
-                    disabled={props.loading}
-                    loading={props.loading}
-                >
-                    {t('Load more')}
-                </Button>
-            </div>
-            : null
-    }
+        : <div className={styles.paginationContainer}>
+            <Button
+                onClick={onLoadMore}
+                disabled={loading}
+                loading={loading}
+            >
+                {t('Load more')}
+            </Button>
+        </div>
 }
 
-function mapStateToProps(store: Store, ownProps: PaginationOwnProps): PaginationStateProps {
-    const bc = store.screen.bo.bc[ownProps.bcName]
-    return {
-        hasNext: bc?.hasNext,
-        page: bc?.page,
-        loading: bc?.loading,
-        widgetName: ownProps.widgetName
-    }
-}
-
-function mapDispatchToProps(dispatch: Dispatch): PaginationDispatchProps {
-    return {
-        changePage: (bcName: string, page: number) => {
-            dispatch($do.bcChangePage({bcName, page}))
-        },
-        loadMore: (bcName: string, widgetName: string) => {
-            dispatch($do.bcLoadMore({bcName: bcName, widgetName: widgetName}))
-        }
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Pagination)
+export default React.memo(Pagination)
