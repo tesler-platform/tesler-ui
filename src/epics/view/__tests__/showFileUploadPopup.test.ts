ed6a3ff854d703da175ed76903990151daae2c9f
dergash <dergachev@hotmail.com>
2020-10-12 02:18:51 +0300

    `showFileUploadPopup` tests

---
+++ src/epics/view/__tests__/showFileUploadPopup.test.ts
@@ -0,0 +1,28 @@
+import {showFileUploadPopup} from '../showFileUploadPopup'
+import {mockStore} from '../../../tests/mockStore'
+import {testEpic} from '../../../tests/testEpic'
+import {$do, types as coreActions} from '../../../actions/actions'
+import {OperationTypeCrud} from '../../../interfaces/operation'
+import {ActionsObservable} from 'redux-observable'
+
+describe('showFileUploadPopup', () => {
+    const store = mockStore()
+    it('fires `bcChangeCursors` and `showFileUploadPopup`', () => {
+        const action = $do.sendOperation({
+            bcName: 'bcExample',
+            operationType: OperationTypeCrud.fileUpload,
+            widgetName: 'test-widget'
+        })
+        testEpic(showFileUploadPopup(ActionsObservable.of(action), store), (result) => {
+            expect(result.length).toBe(2)
+            expect(result[0]).toEqual(expect.objectContaining({
+                type: coreActions.bcChangeCursors,
+                payload: { cursorsMap: { bcExample: null } }
+            }))
+            expect(result[1]).toEqual(expect.objectContaining({
+                type: coreActions.showFileUploadPopup,
+                payload: { widgetName: 'test-widget' }
+            }))
+        })
+    })
+})
