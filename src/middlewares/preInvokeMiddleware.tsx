d25b1ad0f0fa0675904ebb7fe25421736ffcfa19
dergash <dergachev@hotmail.com>
2020-10-30 22:40:37 +0300

    `preInvokeMiddleware` tests

--- src/middlewares/preInvokeMiddleware.tsx
+++ src/middlewares/preInvokeMiddleware.tsx
@@ -1,3 +1,20 @@
+/*
+ * TESLER-UI
+ * Copyright (C) 2018-2020 Tesler Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * Process preInvoke operation before action sendOperation
  */
@@ -12,15 +29,13 @@ const preInvokeAction = ({getState, dispatch}: MiddlewareAPI<Dispatch<AnyAction>
     (action: AnyAction) => {
         if (action.type === types.sendOperation) {
             const state = getState()
-            const operationType = action.payload.operationType
-            const widgetName = action.payload.widgetName
-            const bcName = state.view.widgets.find(widgetItem => widgetItem.name === widgetName).bcName
+            const { operationType, widgetName, confirm } = action.payload
+            const bcName = state.view.widgets.find(widgetItem => widgetItem.name === widgetName)?.bcName
             const bcUrl = buildBcUrl(bcName, true)
             const rowMeta = bcUrl && state.view.rowMeta[bcName]?.[bcUrl]
-            const actions = flattenOperations(rowMeta.actions)
-            const preInvoke = actions.find(item => item.type === operationType)?.preInvoke
-
-            return preInvoke && !action.payload.confirm
+            const actions = rowMeta && flattenOperations(rowMeta.actions)
+            const preInvoke = actions?.find(item => item.type === operationType)?.preInvoke
+            return preInvoke && !confirm
                 ? next($do.processPreInvoke({
                     bcName,
                     operationType,
