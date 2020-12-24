ae2ccb7750cb20f2b2c7a64f4e5de19773ce6911
dergash <dergachev@hotmail.com>
2020-12-24 09:43:27 +0300

    `showAssocPopup` epic will crash when Tesler API returns null instead of empty array, which it will for SQL BC

---
+++ src/epics/view/__tests__/showAssocPopup.test.ts
@@ -0,0 +1,243 @@
+/*
+ * TESLER-UI
+ * Copyright (C) 2018-2020 Tesler Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {showAssocPopup} from '../showAssocPopup'
+import {Store} from 'redux'
+import {ActionsObservable} from 'redux-observable'
+import {Store as CoreStore} from '../../../interfaces/store'
+import {mockStore} from '../../../tests/mockStore'
+import {WidgetTableMeta, WidgetTypes} from '../../../interfaces/widget'
+import {FieldType} from '../../../interfaces/view'
+import {$do} from '../../../actions/actions'
+import {testEpic} from '../../../tests/testEpic'
+
+describe('showAssocPopup', () => {
+    let store: Store<CoreStore> = null
+
+    beforeAll(() => {
+        store = mockStore()
+        store.getState().screen.screenName = 'test'
+        store.getState().screen.bo.bc.bcExample = bcExample
+        store.getState().screen.bo.bc.bcParent = getBcParent()
+        store.getState().view.widgets = [getWidgetMeta()]
+        store.getState().view.pendingDataChanges = getPendingData()
+        store.getState().data = getData()
+    })
+
+    afterEach(() => {
+        store.getState().view.widgets = [getWidgetMeta()]
+        store.getState().view.pendingDataChanges = getPendingData()
+        store.getState().data = getData()
+        store.getState().screen.bo.bc.bcParent = getBcParent()
+    })
+
+    it('fires only for AssocListPopup', () => {
+        const missingCalleeBcName = $do.showViewPopup({ bcName: bcExample.name })
+        const missingCalleeBcNameEpic = showAssocPopup(ActionsObservable.of(missingCalleeBcName), store)
+        testEpic(missingCalleeBcNameEpic, (result) => {
+            expect(result).toHaveLength(0)
+        })
+        const missingAssociateFieldKey = $do.showViewPopup({ bcName: bcExample.name, calleeBCName: bcParent.name })
+        const missingAssociateFieldKeyEpic = showAssocPopup(ActionsObservable.of(missingAssociateFieldKey), store)
+        testEpic(missingAssociateFieldKeyEpic, (result) => {
+            expect(result).toHaveLength(0)
+        })
+    })
+
+    // TODO: This behaviour does not make any sense; we should check if it should not fire
+    // for widgets without options
+    it('does not fire for missing widgets or widgets with options but not full hierarchies (sic)', () => {
+        store.getState().view.widgets[0] = {
+            ...getWidgetMeta(),
+            options: {}
+        }
+        const notFullHierarchy = $do.showViewPopup({
+            bcName: bcExample.name,
+            calleeBCName: bcParent.name,
+            associateFieldKey: 'name'
+        })
+        const notFullHierarchyEpic = showAssocPopup(ActionsObservable.of(notFullHierarchy), store)
+        testEpic(notFullHierarchyEpic, (result) => {
+            expect(result).toHaveLength(0)
+        })
+        const missingWidget = $do.showViewPopup({
+            bcName: 'bcMissing',
+            calleeBCName: bcParent.name,
+            associateFieldKey: 'name'
+        })
+        const missingWidgetEpic = showAssocPopup(ActionsObservable.of(missingWidget), store)
+        testEpic(missingWidgetEpic, (result) => {
+            expect(result).toHaveLength(0)
+        })
+    })
+
+    it('does not fire for missing cursor, pending change for BC or missing field in pending change', () => {
+        store.getState().screen.bo.bc[bcParent.name].cursor = null
+        const missingCursor = $do.showViewPopup({
+            bcName: bcExample.name,
+            calleeBCName: bcParent.name,
+            associateFieldKey: 'name'
+        })
+        const notFullHierarchyEpic = showAssocPopup(ActionsObservable.of(missingCursor), store)
+        testEpic(notFullHierarchyEpic, (result) => {
+            expect(result).toHaveLength(0)
+        })
+        const missingField = $do.showViewPopup({
+            bcName: bcExample.name,
+            calleeBCName: bcParent.name,
+            associateFieldKey: 'missing'
+        })
+        const missingFieldEpic = showAssocPopup(ActionsObservable.of(missingField), store)
+        testEpic(missingFieldEpic, (result) => {
+            expect(result).toHaveLength(0)
+        })
+        const missingBc = $do.showViewPopup({
+            bcName: bcExample.name,
+            calleeBCName: 'bcMissing',
+            associateFieldKey: 'name'
+        })
+        const missingBcEpic = showAssocPopup(ActionsObservable.of(missingBc), store)
+        testEpic(missingBcEpic, (result) => {
+            expect(result).toHaveLength(0)
+        })
+    })
+
+    it('dispatches `changeDataItems` to init `_associate` and `_value` for popup BC from callee BC pending changes', () => {
+        store.getState().view.widgets[0] = {
+            ...getWidgetMeta(),
+            options: undefined
+        }
+        delete store.getState().data[bcParent.name]
+        const action = $do.showViewPopup({
+            bcName: bcExample.name,
+            calleeBCName: bcParent.name,
+            associateFieldKey: 'name'
+        })
+        const epic = showAssocPopup(ActionsObservable.of(action), store)
+        testEpic(epic, (result) => {
+            const expectedAction = $do.changeDataItems({
+                bcName: bcExample.name,
+                cursors: ['300', '400'],
+                dataItems: [
+                    {
+                        id: '300',
+                        _associate: true,
+                        _value: 'XXX-1'
+                    },
+                    {
+                        id: '400',
+                        _associate: true,
+                        _value: 'XXX-2'
+                    }
+                ]
+            })
+            expect(result[0]).toEqual(expect.objectContaining(expectedAction))
+        })
+    })
+
+    it('set `_associate` false for records missing in pending changes', () => {
+        const action = $do.showViewPopup({
+            bcName: bcExample.name,
+            calleeBCName: bcParent.name,
+            associateFieldKey: 'name'
+        })
+        const epic = showAssocPopup(ActionsObservable.of(action), store)
+        testEpic(epic, (result) => {
+            const expectedAction = $do.changeDataItems({
+                bcName: bcExample.name,
+                cursors: ['300', '400', '555'],
+                dataItems: [
+                    {
+                        id: '300',
+                        _associate: true,
+                        _value: 'XXX-1'
+                    },
+                    {
+                        id: '400',
+                        _associate: true,
+                        _value: 'XXX-2'
+                    },
+                    {
+                        id: '555',
+                        _associate: false
+                    }
+                ]
+            })
+            expect(result[0]).toEqual(expect.objectContaining(expectedAction))
+        })
+    })
+})
+
+function getWidgetMeta(): WidgetTableMeta {
+    return {
+        name: 'widget-example',
+        type: WidgetTypes.AssocListPopup,
+        title: null,
+        bcName: 'bcExample',
+        position: 1,
+        gridWidth: null,
+        fields: [{
+            key: 'name',
+            title: 'Test Column',
+            type: FieldType.input
+        }],
+        options: {
+            hierarchyFull: true
+        }
+    }
+}
+
+const bcParent = {
+    name: 'bcParent',
+    parentName: null as string,
+    url: 'bcParent/:id',
+    cursor: '1',
+    loading: false
+}
+
+function getBcParent() {
+    return { ...bcParent }
+}
+
+const bcExample = {
+    name: 'bcExample',
+    parentName: bcParent.name,
+    url: 'bcParent/:id/bcExample/:id',
+    cursor: '1',
+    loading: false
+}
+
+function getPendingData() {
+    return {
+        [bcParent.name]: {
+           [bcParent.cursor]: {
+               id: bcParent.cursor,
+               name: [{ id: '300', value: 'XXX-1' }, { id: '400', value: 'XXX-2' }]
+           }
+        }
+    }
+}
+
+function getData() {
+    return {
+        [bcParent.name]: [{
+            vstamp: 0,
+            id: bcParent.cursor,
+            name: [{ id: '300', value: 'XXX-99' }, { id: '400', value: 'XXX-2' }, { id: '555', value: 'XXX-3' }]
+        }]
+    }
+}
---
+++ src/epics/view/showAssocPopup.ts
@@ -0,0 +1,78 @@
+/*
+ * TESLER-UI
+ * Copyright (C) 2018-2020 Tesler Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Observable} from 'rxjs'
+import {Store} from 'redux'
+import {Epic, types, $do, AnyAction, ActionsMap} from '../../actions/actions'
+import {Store as CoreStore} from '../../interfaces/store'
+import {MultivalueSingleValue, PendingDataItem} from '../../interfaces/data'
+import {WidgetTypes} from '../../interfaces/widget'
+
+export const showAssocPopup: Epic = (action$, store) => action$.ofType(types.showViewPopup)
+.filter(action => !!(action.payload.calleeBCName && action.payload.associateFieldKey))
+.mergeMap((action) => {
+    return showAssocPopupEpicImpl(action, store)
+})
+
+export function showAssocPopupEpicImpl(action: ActionsMap['showViewPopup'], store: Store<CoreStore, AnyAction>) {
+    const {bcName, calleeBCName} = action.payload
+
+    const state = store.getState()
+
+    const assocWidget = state.view.widgets.find((widget) => widget.bcName === bcName && widget.type === WidgetTypes.AssocListPopup)
+    const calleeCursor = state.screen.bo.bc[calleeBCName]?.cursor
+    const calleePendingChanges = state.view.pendingDataChanges[calleeBCName]?.[calleeCursor]
+    const assocFieldKey = action.payload.associateFieldKey
+    const assocFieldChanges = (calleePendingChanges?.[assocFieldKey] as MultivalueSingleValue[])
+    const somethingMissing = !assocWidget
+        || !calleePendingChanges
+        || !assocFieldChanges
+        || !assocFieldChanges
+    if (somethingMissing || assocWidget.options && !assocWidget.options.hierarchyFull) {
+        return Observable.empty<never>()
+    }
+
+    const popupInitPendingChanges: Record<string, PendingDataItem> = {}
+
+    assocFieldChanges.forEach((record) => {
+        popupInitPendingChanges[record.id] = {
+            id: record.id,
+            _associate: true,
+            _value: record.value
+        }
+    })
+
+    const calleeData = state.data[calleeBCName]?.find((dataRecord) => dataRecord.id === calleeCursor)
+    const assocIds = (calleeData?.[assocFieldKey] as MultivalueSingleValue[])?.map((recordId) => recordId.id)
+    const assocPendingIds = assocFieldChanges.map((recordId) => recordId.id)
+    if (assocIds) {
+        assocIds.forEach((recordId) => {
+            if (!assocPendingIds.includes(recordId)) {
+                popupInitPendingChanges[recordId] = {
+                    id: recordId,
+                    _associate: false
+                }
+            }
+        })
+    }
+
+    return Observable.of($do.changeDataItems({
+        bcName,
+        cursors: Object.keys(popupInitPendingChanges),
+        dataItems: Object.values(popupInitPendingChanges)
+    }))
+}
