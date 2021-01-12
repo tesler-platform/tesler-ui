8a6579eb932e9a9181720a70b75295ff604e1c29
dergash <dergachev@hotmail.com>
2021-01-12 20:13:34 +0300

    `loginDone` router epic tests

---
+++ src/epics/router/loginDone.ts
@@ -0,0 +1,52 @@
+/*
+ * TESLER-UI
+ * Copyright (C) 2018-2021 Tesler Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Observable} from 'rxjs'
+import {Store} from 'redux'
+import {Epic, types, AnyAction, ActionsMap, $do} from '../../actions/actions'
+import {Store as CoreStore} from '../../interfaces/store'
+import {RouteType} from '../../interfaces/router'
+
+/**
+ * Fires `selectScreen` or `selectScreenFail` to set requested in url screen as active
+ * after succesful login.
+ *
+ * For server-side router fires `handleRouter` instead.
+ *
+ * @param action$ loginDone
+ */
+export const loginDone: Epic = (action$, store) => action$.ofType(types.loginDone)
+.switchMap(action => {
+    return loginDoneImpl(action, store)
+})
+
+export function loginDoneImpl(action: ActionsMap['loginDone'], store: Store<CoreStore>) {
+    const state = store.getState()
+
+    if (state.router.type === RouteType.router) {
+        return Observable.of($do.handleRouter(state.router))
+    }
+
+    const nextScreenName = state.router.screenName
+    const nextScreen = state.session.screens.find(item => nextScreenName
+            ? item.name === nextScreenName
+            : item.defaultScreen
+        ) || state.session.screens[0]
+    return nextScreen
+        ? Observable.of<AnyAction>($do.selectScreen({ screen: nextScreen }))
+        : Observable.of<AnyAction>($do.selectScreenFail({ screenName: nextScreenName }))
+}
