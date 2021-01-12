dee515bac26b1787fd74b1c36d871d6050acfb87
dergash <dergachev@hotmail.com>
2021-01-12 19:28:36 +0300

    `hierarchyDisableParent` widget option in `<FullHierarchyTable />` erroneously checked `noChildren` flag only among items of current depth allowing nodes with children to be selected despite the widget flag

--- src/components/FullHierarchyTable/FullHierarchyTable.test.tsx
+++ src/components/FullHierarchyTable/FullHierarchyTable.test.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import {shallow} from 'enzyme'
 import {Table} from 'antd'
 import {FullHierarchyTable, FullHierarchyTableAllProps, FullHierarchyTableOwnProps, FullHierarchyDataItem} from './FullHierarchyTable'
-import {WidgetTypes} from 'interfaces/widget'
-import {FieldType} from 'interfaces/view'
+import {WidgetTypes, WidgetTableMeta} from 'interfaces/widget'
+import {FieldType} from '../../interfaces/view'
 import {BcFilter, FilterType} from '../../interfaces/filters'
-
 
 describe('FullHierarchyTable test', () => {
     const toHideOwnProps: FullHierarchyTableOwnProps = {
@@ -56,34 +55,87 @@ describe('FullHierarchyTable test', () => {
 })
 
 describe('FullHierarchyTable test', () => {
+
     const toHideOwnProps: FullHierarchyTableOwnProps = {
         selectable: true,
         depth: 1,
         parentId: '0',
         assocValueKey: 'name',
         searchPlaceholder: 'input value',
-        meta: {
-            name: 'HierarcyPopup',
-            title: 'FullHierarhy',
-            type: WidgetTypes.DataGrid as WidgetTypes.List | WidgetTypes.DataGrid,
-            position: 1,
-            bcName: 'bcName',
-            gridWidth: 12,
-            fields: [
-                {
-                    title: 'Name',
-                    key: 'name',
-                    type: FieldType.input
-                },
-                {
-                    title: 'Description',
-                    key: 'desc',
-                    type: FieldType.input
-                }
-            ]
-        }
+        meta: getWidgetMeta()
     }
-    const dataItems: FullHierarchyDataItem[] = [
+
+    const prefilters: BcFilter[] = [
+        {
+            type: FilterType.contains,
+            fieldName: 'desc',
+            value: ['description']
+        }
+    ]
+    const props: FullHierarchyTableAllProps = {
+        meta: toHideOwnProps.meta,
+        data: getDataItems(),
+        loading: false,
+        pendingChanges: {},
+        bcFilters: prefilters,
+        rowMetaFields: [{currentValue: '11111',key: 'desc'}],
+        onSelect: jest.fn(),
+        onDeselectAll: jest.fn(),
+        onSelectAll: jest.fn(),
+        onSelectFullTable: jest.fn(),
+        addFilter: jest.fn(),
+        removeFilter: jest.fn(),
+    }
+
+    it('filter operation column with test data', () => {
+        const wrapper = shallow(<FullHierarchyTable {...props}/>)
+        expect(wrapper.find(Table).length).toEqual(1)
+        expect(wrapper.find(Table).props().columns
+            .find(i => i.key === props.meta.fields[0].key))
+        expect(wrapper.find(Table).contains('third description'))
+    })
+
+    it('allow selecting only for leaves with `hierarchyDisableParent` flag', () => {
+        const onRowMock = jest.fn()
+        const component = <FullHierarchyTable
+                {...props}
+                meta={{ ...getWidgetMeta(), options: { hierarchyDisableParent: true} }}
+                onRow={onRowMock}
+            />
+        const wrapper = shallow(component)
+        const selectHandler = wrapper.find(Table).prop('onRow')
+        expect(selectHandler({ id: '1', vstamp: 0 }, 0)).toBe(undefined)
+        expect(onRowMock).toBeCalledTimes(0)
+        selectHandler({ id: '1', vstamp: 0, noChildren: true }, 0)
+        expect(onRowMock).toBeCalledTimes(1)
+    })
+})
+
+function getWidgetMeta(): WidgetTableMeta {
+    return {
+        name: 'HierarcyPopup',
+        title: 'FullHierarhy',
+        type: WidgetTypes.DataGrid as WidgetTypes.List | WidgetTypes.DataGrid,
+        position: 1,
+        bcName: 'bcName',
+        gridWidth: 12,
+        fields: [
+            {
+                title: 'Name',
+                key: 'name',
+                type: FieldType.input
+            },
+            {
+                title: 'Description',
+                key: 'desc',
+                type: FieldType.input
+            }
+        ]
+    }
+}
+
+function getDataItems(): FullHierarchyDataItem[] {
+    return [
         {
             _associate: true,
             id: '1',
@@ -115,35 +167,4 @@ describe('FullHierarchyTable test', () => {
             ['desc']: 'third description'
         }
     ]
-    const prefilters: BcFilter[] = [
-        {
-            type: FilterType.contains,
-            fieldName: 'desc',
-            value: ['description']
-        }
-    ]
-    const props: FullHierarchyTableAllProps = {
-        meta: toHideOwnProps.meta,
-        data: dataItems,
-        loading: false,
-        pendingChanges: {},
-        bcFilters: prefilters,
-        rowMetaFields: [{currentValue: '11111',key: 'desc'}],
-        onSelect: jest.fn(),
-        onDeselectAll: jest.fn(),
-        onSelectAll: jest.fn(),
-        onSelectFullTable: jest.fn(),
-        addFilter: jest.fn(),
-        removeFilter: jest.fn(),
-    }
-
-    it('filter operation column with test data', () => {
-        const wrapper = shallow(<FullHierarchyTable {...props}/>)
-        expect(wrapper.find(Table).length).toEqual(1)
-        expect(wrapper.find(Table).props().columns
-            .find(i => i.key === props.meta.fields[0].key))
-        expect(wrapper.find(Table).contains('third description'))
-
-
-    })
-})
+}
