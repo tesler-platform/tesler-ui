b3313717ee0483f5a8ef0dbb27d46efad889373f
Fedor Gubarev <gubarevfyodor@gmail.com>
2021-03-29 21:12:12 +0300

    Pending data in associated popup is removed by forceActive performing (#622)

--- src/reducers/view.ts
+++ src/reducers/view.ts
@@ -5,6 +5,7 @@ import { Store } from '../interfaces/store'
 import { OperationTypeCrud } from '../interfaces/operation'
 import { buildBcUrl } from '../utils/strings'
 import i18n from 'i18next'
+import { MultivalueFieldMeta, WidgetFormField, WidgetListField } from '@tesler-ui/schema'
 
 const initialState: ViewState = {
     id: null,
@@ -136,7 +137,7 @@ export function view(state = initialState, action: AnyAction, store: Store): Vie
             const newPendingChangesDiff: PendingDataItem = {}
             const forceActiveFieldKeys: string[] = []
 
-            // приведем значения переданные в forcedValue в вид дельты изменений
+            // convert the values passed to forceValue to the form of a delta of changes
             rowMeta.fields.forEach(field => {
                 rowMetaForcedValues[field.key] = field.currentValue
                 if (field.forceActive) {
@@ -145,21 +146,56 @@ export function view(state = initialState, action: AnyAction, store: Store): Vie
             })
 
             const consolidatedFrontData: PendingDataItem = { ...currentRecordData, ...state.pendingDataChanges[bcName][cursor] }
-            // вычислим "разницу" между консолид.данными и полученными forcedValue's в пользу последних
+            // calculate the "difference" between the consolidated data and the received forcedValue's in favor of the latter
             Object.keys(consolidatedFrontData).forEach(key => {
                 if (rowMetaForcedValues[key] !== undefined && consolidatedFrontData[key] !== rowMetaForcedValues[key]) {
                     newPendingChangesDiff[key] = rowMetaForcedValues[key]
                 }
             })
 
-            // консолидация полученной разницы с актуальной дельтой
+            // consolidation of the "difference" with the current delta
             const newPendingDataChanges = { ...state.pendingDataChanges[bcName][cursor], ...newPendingChangesDiff }
 
-            // отразим в списке обработанных forceActive полей - те что содержатся в новой дельте
+            // show in the list of processed forceActive fields - those that are contained in the new delta
             forceActiveFieldKeys.forEach(key => {
                 if (newPendingDataChanges[key] !== undefined) {
                     handledForceActive[key] = newPendingDataChanges[key]
                 }
+            })
+
+            // find fields keys which may associate popup
+            const forcedKeysForCheck = Object.entries(newPendingDataChanges)
+                .filter(([key, value]) => {
+                    if (Array.isArray(value) && value.length === 0) {
+                        return true
+                    }
+                    return !value
+                })
+                .map(([key, value]) => key)
+                .filter(key => forceActiveFieldKeys.includes(key))
+
+            // find widget related to `forcedKeysForCheck`
+            const widget = state.widgets.find(
+                i =>
+                    i.bcName === bcName &&
+                    // due to we cannot find by widget name
+                    (i.fields as Array<WidgetListField | WidgetFormField>).filter(field => forcedKeysForCheck.includes(field.key))
+                        .length === forcedKeysForCheck.length
+            )
+
+            // find popupBcNames among `forcedKeysForCheck`
+            const popupBcNames = (widget.fields as Array<WidgetListField | WidgetFormField>)
+                .filter(field => forcedKeysForCheck.includes(field.key) && 'popupBcName' in field)
+                .map(i => (i as MultivalueFieldMeta).popupBcName)
+
+            // clear pending changes related to found `popupBcNames`
+            const clearedPopupsPendingChanges: {
+                [bcName: string]: {
+                    [cursor: string]: PendingDataItem
+                }
+            } = {}
+            popupBcNames.forEach(i => {
+                clearedPopupsPendingChanges[i] = {}
             })
 
             return {
@@ -176,6 +212,7 @@ export function view(state = initialState, action: AnyAction, store: Store): Vie
                 },
                 pendingDataChanges: {
                     ...state.pendingDataChanges,
+                    ...clearedPopupsPendingChanges,
                     [bcName]: {
                         ...(state.pendingDataChanges[bcName] || {}),
                         [cursor]: newPendingDataChanges
