5fef21cc54b8c67a2c7c9bed170fa1d8cbf1d276
dergash <dergachev@hotmail.com>
2021-04-22 21:28:18 +0300

    extract `<RowOperationsButton />` from `<TableWidget />`; reuse `<RowOperationsButton />` on `<HierarchyTable />`

---
+++ src/components/RowOperations/RowOperationsButton.tsx
@@ -0,0 +1,120 @@
+import React, { MutableRefObject } from 'react'
+import { Dropdown, Icon } from 'antd'
+import { WidgetTableMeta } from '../../interfaces/widget'
+import styles from './RowOperationsButton.less'
+import { useDispatch } from 'react-redux'
+import { $do } from '../../actions/actions'
+import { DataItem } from '@tesler-ui/schema'
+import RowOperationsMenu from './RowOperationsMenu'
+import { useRowMenuInstance } from '../../hooks/useRowMenu'
+
+/**
+ * {@link RowOperationsMenu | RowOperationsMenu} properties
+ */
+interface RowOperationsButtonProps {
+    /**
+     * Widget meta description
+     */
+    meta: WidgetTableMeta
+    /**
+     * Use when business component differs from widget's (e.g. hierarchies nested level)
+     */
+    bcName?: string
+    /**
+     * Common parent for {@link Table | Table} and {@link RowOperationsButton | RowOperationsButton}
+     */
+    parent: MutableRefObject<HTMLElement>
+}
+
+/**
+ * {@link RowOperationsButton | RowOperationsButton} instance
+ */
+export interface RowOperationsButtonInstance {
+    setRow: (record: DataItem, e: React.MouseEvent<HTMLElement>) => void
+    containerRef: MutableRefObject<HTMLElement>
+}
+
+/**
+ * Button that shows available operations for the row
+ *
+ * On click fetches row meta for clicked row by dispatching
+ * {@link ActionPayloadTypes.bcSelectRecord | bcSelectRecord} and shows {@link RowOperationsMenu | RowOperationsMenu}
+ *
+ * {@link RowOperationsButtonInstance.setRow | setRow} can be used to dynamically place the button
+ * next to hovered row.
+ *
+ * @param props - Component properties
+ * @param ref - Assigned reference will receive component instance
+ */
+export const RowOperationsButton: React.ForwardRefRenderFunction<RowOperationsButtonInstance, RowOperationsButtonProps> = (
+    { meta, parent, ...props }: RowOperationsButtonProps,
+    ref
+) => {
+    const dispatch = useDispatch()
+    /**
+     * Nested hierarchies might pass bcName directly
+     */
+    const bcName = props.bcName || meta.bcName
+    const containerRef = React.useRef<HTMLDivElement>()
+    const containerCurrent = containerRef.current
+    const [selectedRow, setSelectedRow] = React.useState('')
+    const [showMenu, setShowMenu] = React.useState(false)
+
+    /**
+     * Fetches row meta to get
+     */
+    const handleFetchMeta = React.useCallback(() => {
+        dispatch($do.bcSelectRecord({ bcName, cursor: selectedRow }))
+    }, [bcName, selectedRow, dispatch])
+
+    /**
+     * Hides the button
+     *
+     * @param force - Hides the button even if menu is open
+     */
+    const handleMouseLeave = (force?: boolean) => {
+        if (containerRef.current && (!showMenu || force)) {
+            containerRef.current.style.display = 'none'
+        }
+    }
+
+    /**
+     * Links menu visibility to the state and hides the button on closing menu (???)
+     *
+     * @param visibility
+     */
+    const handleVisibleChange = (visibility: boolean) => {
+        setShowMenu(visibility)
+        if (!visibility) {
+            handleMouseLeave(true)
+        }
+    }
+
+    /**
+     * Anchor button popup to container element
+     */
+    const handlePopupContainer = React.useCallback(() => {
+        return containerCurrent
+    }, [containerCurrent])
+
+    /**
+     * Exposes `setRow` for component instance
+     */
+    useRowMenuInstance(ref, parent, containerRef, showMenu, setSelectedRow, handleMouseLeave)
+
+    return (
+        <div ref={containerRef} className={styles.floatMenu}>
+            <Dropdown
+                placement="bottomRight"
+                trigger={['click']}
+                overlay={<RowOperationsMenu meta={meta} bcName={bcName} />}
+                onVisibleChange={handleVisibleChange}
+                getPopupContainer={handlePopupContainer}
+            >
+                <Icon type="ellipsis" className={styles.dots} onClick={handleFetchMeta} />
+            </Dropdown>
+        </div>
+    )
+}
+
+export default React.forwardRef(RowOperationsButton)
